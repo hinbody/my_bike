1148219c41bc938fcaef22dac45757b5fbdde616
Harley Inbody <hinbody@gmail.com>
2013-01-03 14:12:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBike::Application.config.secret_token = '4b619b02db4aaa4c18a7b9f5fe8fc0bf580e916fa1824b188c438039c2ba0b216fc4cf73610cc809bdc8346a7e8c0035bb5c5980264bf5f3fad9129e8ecb3161'
